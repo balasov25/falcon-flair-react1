8445fa4c20b33cf54396af1d8919dfc0cac5d128
balasov25 <orucbalas@gmail.com>
2025-08-19 18:40:04 +0400

    Create MercedesCLE53.jsx

---
+++ src/MercedesCLE53.jsx
@@ -0,0 +1,9 @@
+export default function MercedesCLE53() {
+  return (
+    <div>
+      <h1>Mercedes CLE53 AMG</h1>
+      <p>Yüksek performans ve lüks detaylar.</p>
+      <img src="https://upload.wikimedia.org/wikipedia/commons/3/37/Mercedes_AMG_CLE_53.jpg" width="400" />
+    </div>
+  );
+}
