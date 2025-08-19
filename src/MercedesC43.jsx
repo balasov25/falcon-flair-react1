abb5dfa36f10b7dd3c801a8ba01a00de0df64cbf
balasov25 <orucbalas@gmail.com>
2025-08-19 18:40:21 +0400

    Create MercedesC43.jsx

---
+++ src/MercedesC43.jsx
@@ -0,0 +1,9 @@
+export default function MercedesC43() {
+  return (
+    <div>
+      <h1>Mercedes C43 AMG</h1>
+      <p>Spor sedan ruhunu taşıyan Mercedes.</p>
+      <img src="https://upload.wikimedia.org/wikipedia/commons/2/22/Mercedes-Benz_C43_AMG.jpg" width="400" />
+    </div>
+  );
+}
