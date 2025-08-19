0997bd0b50ce27e88564dc96c5da48172960f3c8
balasov25 <orucbalas@gmail.com>
2025-08-19 18:40:54 +0400

    Create Rox.jsx

---
+++ src/Rox.jsx
@@ -0,0 +1,9 @@
+export default function Rox() {
+  return (
+    <div>
+      <h1>Rox 01 VIP</h1>
+      <p>Özel tasarım ve VIP konforu ile farklı bir deneyim.</p>
+      <img src="https://via.placeholder.com/400x250?text=Rox+01+VIP" width="400" />
+    </div>
+  );
+}
