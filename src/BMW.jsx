e63356f654c36e4b70eea5bae52d646f7978debc
balasov25 <orucbalas@gmail.com>
2025-08-19 18:39:45 +0400

    Create BMW.jsx

---
+++ src/BMW.jsx
@@ -0,0 +1,9 @@
+export default function BMW() {
+  return (
+    <div>
+      <h1>BMW M440i</h1>
+      <p>Güçlü performans, spor tasarım ve konfor bir arada.</p>
+      <img src="https://upload.wikimedia.org/wikipedia/commons/0/0b/2021_BMW_M440i_xDrive_Coupe_Front.jpg" width="400" />
+    </div>
+  );
+}
