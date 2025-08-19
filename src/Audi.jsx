3375b2b7a9e2f033196dd6793485564fcc84a119
balasov25 <orucbalas@gmail.com>
2025-08-19 18:40:42 +0400

    Create Audi.jsx

---
+++ src/Audi.jsx
@@ -0,0 +1,9 @@
+export default function Audi() {
+  return (
+    <div>
+      <h1>Audi RS3</h1>
+      <p>Kompakt boyutta dev performans.</p>
+      <img src="https://upload.wikimedia.org/wikipedia/commons/1/19/Audi_RS3_8Y.jpg" width="400" />
+    </div>
+  );
+}
